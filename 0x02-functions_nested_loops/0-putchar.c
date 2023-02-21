8fbb36c4cd75bc9e9a91d6ed4a4021fc80c02a33
Mohamed Tarek <mohamedtarek4z79.000@gmail.com>
2023-02-22 00:38:02 +0200

    Updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
-*main - Entry point
-*
-*Return: 0 nothing, return void
-*/
-
+ * main - Print _putchar
+ * Return: Always 0
+ */
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
@@ -18,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
